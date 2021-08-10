bc0fafaacfb85e424e7ffd2f2703c288e8584f97
Shuji <tokyosheep@hotmail.com>
2021-08-10 15:19:00 +0900

    first commit

---
+++ Desktop/SoundsOfTheCity/src/pages/about.js
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+const About = () =>{
+    return <diov>Home</diov>
+}
+
+export default About;
---
+++ Desktop/SoundsOfTheCity/src/pages/index.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+    background: #921;
+`;
+
+const Home = () =>{
+    return <Wrapper>Home</Wrapper>
+}
+
+export default Home;
